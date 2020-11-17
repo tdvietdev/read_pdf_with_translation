3e56e0adfffd2c09635d00a89832c129f6b9d10a
Trinh Dinh Viet <trinh.dinh.viet@sun-asterisk.com>
2020-11-17 10:37:02 +0700

    change code style

--- src/js/menutemplate.js
+++ src/js/menutemplate.js
@@ -1,92 +1,92 @@
 "use strict";
 
-const { app, dialog } = require('electron');
+const {app, dialog} = require('electron');
 
 exports.buildMenuTemplate = function (win) {
-    return [
+  return [
+    {
+      label: 'File',
+      submenu: [
         {
-            label: 'File',
-            submenu: [
-                {
-                    label: 'Open...',
-                    id: 'file-open',
-                    accelerator: 'CmdOrCtrl+O',
-                    click() {
-                        dialog.showOpenDialog(win, {
-                            properties: ['openFile'],
-                            filters: [
-                                { name: 'PDF Files', extensions: ['pdf'] }
-                            ]
-                        }, (filename) => {
-                            if (filename) {
-                                win.webContents.send('file-open',
-                                    filename.toString())
-                            }
-                        })
-                    }
-                },
-                {
-                    label: 'Print...',
-                    id: 'file-print',
-                    accelerator: 'CmdOrCtrl+P',
-                    enabled: false,
-                    click() {
-                        win.webContents.send('file-print')
-                    }
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Properties...',
-                    id: 'file-properties',
-                    enabled: false,
-                    click() {
-                        win.webContents.send('file-properties')
-                    }
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Close',
-                    id: 'file-close',
-                    enabled: false,
-                    click() {
-                        win.webContents.send('file-close')
-                    }
-                },
-                {
-                    label: 'Exit',
-                    click() {
-                        app.quit()
-                    }
-                }
-            ]
+          label: 'Open...',
+          id: 'file-open',
+          accelerator: 'CmdOrCtrl+O',
+          click() {
+            dialog.showOpenDialog(win, {
+              properties: ['openFile'],
+              filters: [
+                {name: 'PDF Files', extensions: ['pdf']}
+              ]
+            }, (filename) => {
+              if (filename) {
+                win.webContents.send('file-open',
+                    filename.toString())
+              }
+            })
+          }
         },
         {
-            label: 'View',
-            submenu: [
-                {
-                    label: 'Toggle Full Screen',
-                    id: 'view-fullscreen',
-                    enabled: false,
-                    accelerator: 'F11',
-                    click() {
-                        win.webContents.send('view-fullscreen')
-                    }
-                }
-            ]
+          label: 'Print...',
+          id: 'file-print',
+          accelerator: 'CmdOrCtrl+P',
+          enabled: false,
+          click() {
+            win.webContents.send('file-print')
+          }
         },
         {
-            label: 'Help',
-            submenu: [
-                {
-                    label: 'About',
-                    id: 'about'
-                }
-            ]
+          type: 'separator'
+        },
+        {
+          label: 'Properties...',
+          id: 'file-properties',
+          enabled: false,
+          click() {
+            win.webContents.send('file-properties')
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Close',
+          id: 'file-close',
+          enabled: false,
+          click() {
+            win.webContents.send('file-close')
+          }
+        },
+        {
+          label: 'Exit',
+          click() {
+            app.quit()
+          }
         }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Toggle Full Screen',
+          id: 'view-fullscreen',
+          enabled: false,
+          accelerator: 'F11',
+          click() {
+            win.webContents.send('view-fullscreen')
+          }
+        }
+      ]
+    },
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'About',
+          id: 'about'
+        }
+      ]
+    }
 
-    ];
+  ];
 };
